1ad9bc37449524b6fb65c346f95add5bd2dd7d4f
Daivd <drgdeved@gmail.com>
2026-02-09 23:28:43 +0200

    Create popup form to make new books

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 
+TODO:
+1- make it that when the user clicks on popup background. the popup disappears
+2- make the popup form functional(when the user submits. the data turn to an object inside an array)
+3- display all objects inside the array in <div class="content"> using the placeholder as a blueprint
+
+
+*/
