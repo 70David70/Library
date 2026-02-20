07fd53c2a8b86d7ac28e845e5b3b6e41038c54d4
Daivd <drgdeved@gmail.com>
2026-02-21 00:30:13 +0200

    Make the UI intractive with js

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ TODO:
 
 
 */
+let htmlBody = document.querySelector("body")
+let addBtn = document.querySelector(".add")
+let creatingForm = document.querySelector("#popup-background")
+
+htmlBody.addEventListener("click", (e)=> {
+    let attributeClass = e.target.id;
+
+    if (attributeClass == "add")
+    {
+        creatingForm.classList.toggle("hidden")
+    }
+    else if (attributeClass == "popup-background") {
+        e.target.classList.toggle("hidden")        
+    }
+    else if (attributeClass == "delete-box") {
+        // TODO delete the corresponding book from array
+    }
+    else if (attributeClass == "close-popup") {
+        e.target.closest("#popup-background").classList.toggle("hidden")
+    }
+})
+htmlBody.addEventListener("mouseover", (e)=> {
+    const book = e.target.closest("#placeholder-book")
+    if (!book) return;
+    book.querySelector(".book-overlay").classList.remove("hidden");
+})
+htmlBody.addEventListener("mouseout", (e)=> {
+    const book = e.target.closest("#placeholder-book")
+    if (!book) return;
+    book.querySelector(".book-overlay").classList.add("hidden")
+})
