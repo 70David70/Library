0b47913fb676c378cda69265b46cccf431fb732b
David <drgdeved@gmail.com>
2026-03-09 21:41:06 +0200

    Delete todo comment in script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/* 
-TODO:
-1- make it that when the user clicks on popup background. the popup disappears
-2- make the popup form functional(when the user submits. the data turn to an object inside an array)
-3- display all objects inside the array in <div class="content"> using the placeholder as a blueprint
-
-
-*/
 let htmlBody = document.querySelector("body")
 let addBtn = document.querySelector(".add")
 let creatingForm = document.querySelector("#popupBackground")
